2656ae3f94a2b6897a3632047b76c83d30ed67c8
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-09-23 17:31:11 +0100

    feat: implement controller for CoinageCompanyController

---
+++ Coinage/Controllers/CoinageCompanyController.cs
@@ -0,0 +1,33 @@
+﻿using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Coinage.Controllers
+{
+    [Route("api/coinage/[controller]")]
+    public class CoinageCompanyController : Controller
+    {
+
+        private readonly IRepositoryManager _repository;
+        //private IMapper _mapper;
+
+        public CoinageCompanyController(IRepositoryManager repository)
+        {
+            _repository = repository;
+        }
+
+
+        //Get all EcoCompanies
+        [HttpGet]
+        public async Task<IActionResult> GetAllEcoCompanies()
+        {
+            var ecoCompanies = await _repository.CoinageCompanyRepository.GetAllEcoCompaniesAsync(trackChanges: false);
+            //var ecoCompaniesForOutputDto = _mapper.Map<IEnumerable<EcoCompanyForOutputDto>>(ecoCompanies);
+
+            return Ok(ecoCompanies);
+        }
+    }
+}
