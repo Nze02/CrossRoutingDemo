7d8006e047b6a8ba31cbd5bc76bb9470d742cd9b
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-09-23 15:29:55 +0100

    feat: add next project (Coinage) and a model

---
+++ CrossRouting/Controllers/EcoCompanyController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repositories.Contracts;
+
+namespace CrossRouting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EcoCompanyController : ControllerBase
+    {
+        private readonly IRepositoryManager _repository;
+        //private IMapper _mapper;
+
+        public EcoCompanyController(IRepositoryManager repository)
+        {
+            _repository = repository;
+        }
+
+
+        //Get all EcoCompanies
+        [HttpGet]
+        public async Task<IActionResult> GetAllEcoCompanies()
+        {
+            var ecoCompanies = await _repository.CompanyRepository.GetAllEcoCompaniesAsync(trackChanges: false);
+            //var ecoCompaniesForOutputDto = _mapper.Map<IEnumerable<EcoCompanyForOutputDto>>(ecoCompanies);
+
+            return Ok(ecoCompanies);
+        }
+    }
+}
---
+++ Entities/CoinageCompany.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Entities
+{
+    public class CoinageCompany
+    {
+        [Key]
+        [Display(Name = "Company ID")]
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CompanyOID { get; set; } = -1;
+
+        [Required]
+        [StringLength(250)]
+        public string CompanyName { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Street { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [StringLength(50)]
+        public string Zip { get; set; }
+
+        public string Country { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+    }
+}
