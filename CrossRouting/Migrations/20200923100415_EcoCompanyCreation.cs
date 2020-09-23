4ddf680bbb349e3f970b8ae93011694a227bc3d0
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-09-23 11:06:45 +0100

    feat: add configuration and migration for EcoCompany model

---
+++ CrossRouting/Migrations/20200923100415_EcoCompanyCreation.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CrossRouting.Migrations
+{
+    public partial class EcoCompanyCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EcoCompany",
+                columns: table => new
+                {
+                    CompanyOID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyName = table.Column<string>(maxLength: 250, nullable: false),
+                    Street = table.Column<string>(maxLength: 250, nullable: false),
+                    City = table.Column<string>(maxLength: 100, nullable: false),
+                    State = table.Column<string>(nullable: false),
+                    Zip = table.Column<string>(maxLength: 50, nullable: true),
+                    Country = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EcoCompany", x => x.CompanyOID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EcoCompany");
+        }
+    }
+}
---
+++ Entities/EcoCompany.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrossRouting.Models
+{
+    public class EcoCompany
+    {
+        [Key]
+        [Display(Name = "Company ID")]
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CompanyOID { get; set; } = -1;
+
+        [Required]
+        [StringLength(250)]
+        public string CompanyName { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Street { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string City { get; set; }
+
+        [Required]
+        public string State { get; set; }
+
+        [StringLength(50)]
+        public string Zip { get; set; }
+
+        public string Country { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+  
+    }
+}
